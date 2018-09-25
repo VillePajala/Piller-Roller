f94443d4b01163afeb9f9439f18e17fe00b7396b
VillePajala <valoraami@gmail.com>
2018-09-25 11:11:58 +0300

    Camera now follows the player

---
+++ Piller Roller/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    private Transform vehicle = null;
+	
+	void Start () {
+
+        this.vehicle = GameObject.Find("Player").transform;
+
+	} // Start
+	
+	
+	void Update () {
+
+        this.transform.LookAt(this.vehicle);
+		
+	} // Update
+}
